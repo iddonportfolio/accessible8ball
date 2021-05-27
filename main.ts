c0d4cbc74c5ef807e415a47a4e2860939986c809
iddonportfolio <71628001+iddonportfolio@users.noreply.github.com>
2021-05-27 00:00:05 -0600

    Fixed tone-servo command conflict.

--- main.ts
+++ main.ts
@@ -1,20 +1,22 @@
 /**
- * nested if/else statements determine which random number between 1 and 6 was generated
- */
-/**
- * 500ms pause after face to prevent the SHAKE display from instantly overwriting the face since it is continuing in parallel with this code.
+ * rest position (when device not shaken) for arm with no answer at 90 degrees.
  */
 /**
  * when shaken, picks the number one time per shake, but displays your answer twice in case you didn't catch it the first time.
  */
 /**
- * given that tones tend to make the servo go to 0 degrees, tone will be played first and then the actual answer direction given on the servo so that the tone error on servos won't affect the desired answer.
+ * nested if/else statements determine which random number between 1 and 6 was generated
+ */
+/**
+ * forcing the servo pin low (0) with a digital write before a tone is played will keep the tone from being output on the servo pin as well, which causes servo motion glitches.
+ * 
+ * the same process is done for the buzzer, forcing it low (0) whenever a servo command is sent to prevent that from also being heard on the buzzer pin.
+ */
+/**
+ * 500ms pause after face to help prevent the SHAKE display from instantly overwriting the face since it is continuing in parallel with this code.
  */
 /**
  * ERROR state for if somehow no valid number was chosen, for example in case of corruption of memory.
- */
-/**
- * rest position (when device not shaken) for arm with no answer at 90 degrees.
  */
 input.onGesture(Gesture.Shake, function () {
     number = randint(1, 6)
@@ -29,7 +31,9 @@ input.onGesture(Gesture.Shake, function () {
                 # # # # #
                 `)
             basic.pause(500)
+            pins.digitalWritePin(DigitalPin.P1, 0)
             music.playMelody("F A C5 - - - - - ", 120)
+            pins.digitalWritePin(DigitalPin.P0, 0)
             pins.servoWritePin(AnalogPin.P1, 0)
         } else if (number == 2) {
             basic.showLeds(`
@@ -40,8 +44,10 @@ input.onGesture(Gesture.Shake, function () {
                 # # # # #
                 `)
             basic.pause(500)
+            pins.digitalWritePin(DigitalPin.P1, 0)
             music.playMelody("F A C5 - - - - - ", 120)
-            pins.servoWritePin(AnalogPin.P1, 30)
+            pins.digitalWritePin(DigitalPin.P0, 0)
+            pins.servoWritePin(AnalogPin.P1, 45)
         } else if (number == 3) {
             basic.showLeds(`
                 # # . # #
@@ -51,18 +57,10 @@ input.onGesture(Gesture.Shake, function () {
                 . . . . .
                 `)
             basic.pause(500)
-            pins.servoWritePin(AnalogPin.P1, 60)
-        } else if (number == 4) {
-            basic.showLeds(`
-                # # . # #
-                # # . # #
-                . . . . .
-                # # # # #
-                . . . . .
-                `)
-            basic.pause(500)
+            pins.digitalWritePin(DigitalPin.P1, 0)
             music.playMelody("F E F E - - - - ", 120)
-            pins.servoWritePin(AnalogPin.P1, 120)
+            pins.digitalWritePin(DigitalPin.P0, 0)
+            pins.servoWritePin(AnalogPin.P1, 135)
         } else if (number == 5) {
             basic.showLeds(`
                 # # . # #
@@ -72,23 +70,15 @@ input.onGesture(Gesture.Shake, function () {
                 # . . . #
                 `)
             basic.pause(500)
+            pins.digitalWritePin(DigitalPin.P1, 0)
             music.playMelody("D C C - - - - - ", 120)
-            pins.servoWritePin(AnalogPin.P1, 150)
-        } else if (number == 6) {
-            basic.showLeds(`
-                # # . # #
-                # # . # #
-                . . . . .
-                # # # # #
-                # . . . #
-                `)
-            basic.pause(500)
-            music.playMelody("D C C - - - - - ", 120)
+            pins.digitalWritePin(DigitalPin.P0, 0)
             pins.servoWritePin(AnalogPin.P1, 180)
         } else {
             basic.showString("ERROR!")
         }
         basic.pause(5000)
+        pins.digitalWritePin(DigitalPin.P0, 0)
         pins.servoWritePin(AnalogPin.P1, 90)
     }
 })
